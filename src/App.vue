5580e19ae91f42e5641784602f24f73142fde726
tamara.celis <tcelist99@gmail.com>
2022-09-09 12:19:57 -0400

    Filas como componente

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,16 @@
 <template>
   <div id="app" class="container">
-    <form>
-      <h1>Nombre Formulario</h1>
-      <div class="form-row" >
-        <div class="row row-cols-1 row-cols-lg-1 g-2 g-lg-3">
-          <div class="col">
-            <div id="filas-creadas" v-for="(row, index) in rows" :key="index">
-              <form>
-                <div class="form-group col-md-4">
-                  <input type="text" class="form-control" placeholder="Nombre Fila">
-                </div>
-              </form>
-              <br>
-              <div class="p-3 border-solid bg-light rounded container" v-bind="row.fila">
-                <p>Secciones...</p>
-                <br>
-              </div>
-              <hr>
-            </div>
-            <div class="p-3 border-dotted bg-light rounded" > <!-- Hacer el for aquí para que solo se haga ciclo por la fila con el boton -->
-              <div class="container text-center">
-                <button type="button"  class="btn btn-primary btn-circle btn-xl" @click="addRow">
-                  <h1 class="h1-button">+</h1>
-                </button>
-                <p>Añadir Fila</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
+    <Row/>
   </div>
 </template>
 
 <script>
 
+import Row from './components/Row.vue';
 export default {
-  name: "App",
-  
-  data: () => ({
-    rows: [
-      
-    ]
-  }),
+    name: "App",
+    components: { Row }
+}
 
-  methods: {
-    addRow () {
-      this.rows.push({
-        fila: ''
-      })
-    },
-    changeRow (){
-
-    }
-  }
-};
 </script>
 
-<style>
-
-.btn-circle.btn-xl {
-  width: 70px;
-  height: 70px;
-  padding: 10px 16px;
-  border-radius: 35px;
-  font-size: 12px;
-  text-align: center;
-  background: #008A94;
-}
-
-.btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary:visited {
-    background-color: #008A94 !important;
-}
-
-.h1-button {
-  color: black;
-}
-
-.border-dotted{
- border-style: dotted;
- border-color: #BDBBBB;
-}
-</style>
