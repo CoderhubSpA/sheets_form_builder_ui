f18888997dffaf3621f85352bfa5b2374afccef6
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2022-10-08 02:39:26 -0300

    ADD: La configuración de campos utiliza los datos de la API
    Cada campo ahora tiene su configuración cargada desde la API, aunque todavía no están soportados todos los formatos (sólo texto y select).

--- src/components/Toolbox/ConfigDropdownMenu.vue
+++ src/components/Toolbox/ConfigDropdownMenu.vue
@@ -86,18 +86,42 @@
           </div>
 
           <div v-else-if="currentField">
-            <h4>Campo:</h4>
-            <h5>"{{currentField.name}}"</h5>
-            <label for="field-config-name">Nombre del campo</label>
-            <b-form-input
-              id="field-config-name"
-              type="text-input"
-              v-model="currentField.name"
-              placeholder="Ingresa nombre del campo">
-            </b-form-input>
+            <h4 @click="myPrint(currentField)">{{currentField.name + currentField.id}}</h4>
             <br>
+            <div v-for="element in $store.state.api.fields_config" :key="element" style="padding: 0.5em">
+              <label :for="'menu-'+menu_id+'-element-'+element.id">{{ element.name + element.id }}</label>
+              
+              <b-form-checkbox v-if="element.format=='checkbox'"
+                :id="'menu-'+menu_id+'-element-'+element.id"
+                :v-model="$store.state.api.fields_config_values[currentField.id][element.id].values">
+              </b-form-checkbox>
+              <b-form-input v-else-if="element.format=='TEXT'" @click="myPrint($store.state.api.fields_config_values[currentField.id][element.id])"
+                :id="'menu-'+menu_id+'-element-'+element.id"
+                type="text-input"
+                :v-model="$store.state.api.fields_config_values[currentField.id][element.id].values"
+                :placeholder="'Ingresa' + element.name">
+              </b-form-input>
+              <div v-else-if="element.format=='SELECTOR'">
+                <select class="form-select"
+                  :id="'menu-'+menu_id+'-element-'+element.id"
+                >
+                  
+                  <option v-for="option in $store.state.api.fields_config_select[element.id].options" 
+                  :v-model="$store.state.api.fields_config_values[currentField.id][element.id].values"
+                  :value="option"
+                  :key="option.id"
+                  >{{option.name}}</option>
+
+                </select>
+              </div>
+              <b-list-group-item v-else>
+                {{ element }}
+              </b-list-group-item>
+            </div>
+            <!--
             <label for="field-config-required">Requerido</label>
             <b-form-checkbox id="field-config-required" v-model="currentField.required"></b-form-checkbox>
+            -->
             <br>
             <label for="field-config-col-sm">col sm: </label>
             <b-input v-model="currentField.colSm" id="field-config-col-sm" type="number"/>
