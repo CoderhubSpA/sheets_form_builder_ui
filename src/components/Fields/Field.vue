2a182d018e09572f0fd89ca18be5a258cea923f7
Jing <jinghluozeng@gmail.com>
2022-10-13 21:13:02 -0300

    FIX:Ahora hay espacio entre campos dentro de un formulario

--- src/components/Fields/Field.vue
+++ src/components/Fields/Field.vue
@@ -1,45 +1,47 @@
 <template>
   <div class="form-group">
-    <draggable class="card-deck" 
-    :list="fields" group="Fields" @change="onChange" @dragleave.native="dragleave">
+    <draggable class="card-deck" :list="fields" group="Fields" @change="onChange" @dragleave.native="dragleave">
       <transition-group tag="b-row" class="sections">
-        <b-col  v-if="$store.state.tools.hover_fields" key="drop" cols="12">
+        <b-col v-if="$store.state.tools.hover_fields" key="drop" cols="12">
           <div class="p-3 my-2 border-dotted bg-light rounded text-center text-secondary"> Suelta el campo acá</div>
         </b-col>
-
-      <b-col style="margin-bottom: 15px; background-color: lightgray; border-radius: 5px; padding: 8px;" 
-        :cols = "view =='xl' ? (field.colXl ? field.colXl : 12) : 
-                (view == 'md' ? (field.colMd ? field.colMd : 12): 
-                field.colSm ? field.colSm : 12) "
-        
-        v-for="(field, fieldIdx) in sections[idxSection].fields" :key="field.index" :id="`section-${idxSection}-field-${fieldIdx}`" class="form-group">
-        <div class="flex"  @mouseover="field.show=true" @mouseleave="field.show = false">
-          <div class="form-group col-12">
-            <div style="text-align: left !important; margin-bottom: 5px;"
-
-            >
-              <!--<i>{{field.field_type_text}}</i>-->
-              <button type="button" v-if="field.show" style="float:right" class="close-rounded badge border border-light bg-danger p-2" v-b-modal="`modal-borrar-campo-${idxRow}-${idxSection}-${fieldIdx}`">x</button> 
-              <button type="button" v-if="field.show" style="float:right" class="close-rounded badge border border-light bg-info p-2" @click="openFieldConfig(field)">
-                <v-icon class="d-inline-block ml-2 mb-1" :dark="true" name="cog"/>
-              </button>
-              <b-modal :id="`modal-borrar-campo-${idxRow}-${idxSection}-${fieldIdx}`" centered hide-header @ok="deleteField(fieldIdx)"  ok-variant="danger" ok-title="Sí, estoy seguro" cancel-title="Cancelar">
-                <template #default="{ close }">
-                  <div class="container row justify-content-end">
-                    <b-button class="btn btn-close"  @click="close()"> </b-button>
-                    <h5 class="text-center" >¿Está seguro que desea eliminar este campo?</h5>
-                  </div>
-                </template>
-              </b-modal>         
+        <b-col :cols="view =='xl' ? (field.colXl ? field.colXl : 12) : 
+        (view == 'md' ? (field.colMd ? field.colMd : 12): 
+        field.colSm ? field.colSm : 12) " v-for="(field, fieldIdx) in sections[idxSection].fields" :key="field.index"
+          :id="`section-${idxSection}-field-${fieldIdx}`" class="form-group">
+          <div class="flex" @mouseover="field.show=true" @mouseleave="field.show = false"
+            style="margin-bottom: 15px; background-color: lightgray; border-radius: 5px; padding: 8px;">
+            <div class="form-group col-12">
+              <div style="text-align: left !important; margin-bottom: 5px;">
+                <div v-if="field.show">
+                  <button type="button" style="float:right"
+                    class="close-rounded badge border border-light bg-danger p-2"
+                    v-b-modal="`modal-borrar-campo-${idxRow}-${idxSection}-${fieldIdx}`">x</button>
+                  <button type="button" style="float:right" class="close-rounded badge border border-light bg-info p-2"
+                    @click="openFieldConfig(field)">
+                    <v-icon class="d-inline-block ml-2 mb-1" :dark="true" name="cog" />
+                  </button>
+                </div>
+                <b-modal :id="`modal-borrar-campo-${idxRow}-${idxSection}-${fieldIdx}`" centered hide-header
+                  @ok="deleteField(fieldIdx)" ok-variant="danger" ok-title="Sí, estoy seguro" cancel-title="Cancelar">
+                  <template #default="{ close }">
+                    <div class="container row justify-content-end">
+                      <b-button class="btn btn-close" @click="close()"> </b-button>
+                      <h5 class="text-center">¿Está seguro que desea eliminar este campo?</h5>
+                    </div>
+                  </template>
+                </b-modal>
+              </div>
+              <b-input v-model="field.name" type="text" class="form-control" placeholder="Nombre campo" />
             </div>
-            <b-input v-model="field.name" type="text" class="form-control" placeholder="Nombre campo"/>
           </div>
-            
-        </div>
-        <div class="p-3 my-2 border-dotted bg-light rounded text-center text-secondary" v-if="$store.state.tools.hover_fields" key="drop"> Suelta el campo acá</div>
-      </b-col>
-      
 
+        </b-col>
+        <b-col
+          v-if="$store.state.tools.hover_fields && this.$store.state.form.form.rows[idxRow].sections[idxSection].fields.length > 0"
+          key="drop" cols="12">
+          <div class="p-3 my-2 border-dotted bg-light rounded text-center text-secondary"> Suelta el campo acá</div>
+        </b-col>
       </transition-group>
 
     </draggable>
@@ -55,14 +57,14 @@ export default {
   components: {
     draggable
   },
-  props:{
+  props: {
     name: {
       type: String,
       required: false
     },
-    idxRow:{
-      type:Number,
-      required:true
+    idxRow: {
+      type: Number,
+      required: true
     },
     idxSection: {
       type: Number,
@@ -76,7 +78,7 @@ export default {
     fields() {
       return this.$store.state.form.form.rows[this.idxRow].sections[this.idxSection].fields;
     },
-    view(){
+    view() {
       return this.$store.state.form.current_view
     }
   },
@@ -90,7 +92,7 @@ export default {
       this.updateFields(index);
       this.fields.splice(index, 1);
     },
-    updateFields(index){
+    updateFields(index) {
       this.$store.state.api.fields.push(this.fields[index]);
     },
     onChange(event) {
@@ -108,14 +110,12 @@ export default {
     },
     dragleave() {
       this.$store.commit('tools/change_hover', false);
-  
     },
   }
 };
 </script>
   
 <style>
-
 .btn-circle.btn-lg {
   width: 55px;
   height: 55px;
@@ -126,7 +126,10 @@ export default {
   background: #008A94;
 }
 
-.btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary:visited {
+.btn-primary,
+.btn-primary:hover,
+.btn-primary:active,
+.btn-primary:visited {
   background-color: #008A94 !important;
 }
 
@@ -134,7 +137,7 @@ export default {
   color: black;
 }
 
-.border-dotted{
+.border-dotted {
   border-style: dotted;
   border-color: #BDBBBB;
 }
@@ -145,10 +148,8 @@ export default {
   text-align: right;
 }
 
-.close-rounded{
-  width: 2rem; 
-  height:2rem;
+.close-rounded {
+  width: 2rem;
+  height: 2rem;
 }
-
-
 </style>
