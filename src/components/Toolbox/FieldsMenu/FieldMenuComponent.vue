ceb1d8558b92cb76719a1659860f2efcc417fbfa
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2022-12-02 17:10:59 -0300

    REFACTOR: Elige los íconos para cada input desde tools.js

--- src/components/Toolbox/FieldsMenu/FieldMenuComponent.vue
+++ src/components/Toolbox/FieldsMenu/FieldMenuComponent.vue
@@ -6,134 +6,7 @@
         uncommented fields have a placeholder icon -->
         <ul class="list-group list-group-horizontal">
           <li class="list-group-item d-flex align-items-center">
-            <v-icon v-if="format === 'CLP'" dense name="dollar-sign"></v-icon>
-            <!-- <v-icon v-else-if="format === 'DATE'" dense name="calendar"></v-icon> -->
-            <v-icon v-else-if="format === 'DATE'" name="calendar"></v-icon>
-            <v-icon v-else-if="format === 'DATETIME'" name="clock"></v-icon>
-            <!-- <v-icon v-else-if="format === 'DATETIME'" dense name="fa-calendar-clock"></v-icon> -->
-            <v-icon
-              v-else-if="format === 'DOCUMENT'"
-              dense
-              name="file"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'DOCUMENT[EXCEL]'"
-              dense
-              name="file-excel"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'DOCUMENT[IMAGE]'"
-              dense
-              name="file-image"
-            ></v-icon>
-            <!-- <v-icon v-else-if="format === 'FORM'" dense name="file"></v-icon> -->
-            <v-icon
-              v-else-if="format === 'INFO'"
-              dense
-              name="circle-info"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'LATITUDE'"
-              dense
-              name="location-dot"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'LONGITUDE'"
-              dense
-              name="location-dot"
-            ></v-icon>
-            <v-icon v-else-if="format === 'MAPA'" dense name="map"></v-icon>
-            <!-- <v-icon v-else-if="format === 'NUMBER'" dense class="material-icons" name="numbers"></v-icon> -->
-            <!-- <v-icon v-else-if="format === 'NUMBER'" name="input-numeric"></v-icon> -->
-            <v-icon v-else-if="format === 'PASSWORD'" dense name="key"></v-icon>
-            <v-icon
-              v-else-if="format === 'PERCENTAGE'"
-              dense
-              name="percent"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'PERCENTAGE[X100]'"
-              dense
-              name="percent"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'QUESTION'"
-              dense
-              name="question"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'RADIO'"
-              dense
-              name="radio-button-checked"
-            ></v-icon>
-            <!-- <v-icon v-else-if="format === 'RADIO'" dense name="circle-dot"></v-icon> -->
-            <!-- <v-icon v-else-if="format === 'RESPONSE'" dense name="file"></v-icon> -->
-            <v-icon v-else-if="format === 'RUT'" dense name="user"></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR'"
-              dense
-              name="list"
-            ></v-icon>
-            <!-- <v-icon v-else-if="format === 'SELECTOR'" dense name="list-dropdown"></v-icon> -->
-            <v-icon
-              v-else-if="format === 'SELECTOR[1XN][ALL]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[1XN][AVAILABLES]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[1XN][ONLYNEW]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[IMAGELIST]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[MULTIPLE]'"
-              name="list"
-            ></v-icon>
-            <!-- <v-icon v-else-if="format === 'SELECTOR[MULTIPLE]'" dense name="check-square"></v-icon> -->
-            <v-icon
-              v-else-if="format === 'SELECTOR[MULTIPLE][ADVANCED]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[REMOTE]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[REMOTE][MULTIPLE]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SELECTOR[REMOTE][MULTIPLE][ALL]'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon
-              v-else-if="format === 'SiNo'"
-              dense
-              name="check-square"
-            ></v-icon>
-            <v-icon v-else-if="format === 'TEXT'" name="edit"></v-icon>
-            <!-- <v-icon v-else-if="format === 'TEXT'" name="input-text"></v-icon> -->
-            <v-icon
-              v-else-if="format === 'TEXT[AREA]'"
-              dense
-              name="edit"
-            ></v-icon>
-            <v-icon v-else-if="format === 'URL'" dense name="link"></v-icon>
-            <v-icon v-else dense name="file"></v-icon>
+            <v-icon dense :name="selectVIconName(format)"></v-icon>
           </li>
           <li class="list-group-item w-100">
             <div class="card-title">{{ text }}</div>
@@ -164,6 +37,11 @@ export default {
   computed: {
     format() {
       return this.config[this.format_config_id].id;
+    },
+  },
+  methods: {
+    selectVIconName(fieldFormat) {
+      return this.$store.getters["tools/selectVIconName"](fieldFormat);
     },
   },
   data() {
